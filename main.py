f3d07bbd96a7ad2f1367baa7dc9dcb3e4e9c7301
Fatima <76473050+fabacha@users.noreply.github.com>
2023-08-28 11:54:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,125 +1,163 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Python version: 3.6
+import random
 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+from torchvision import datasets, transforms
 import torch
-from torch import nn
-import torch.nn.functional as F
+import os
+
+from utils.sampling import mnist_iid, mnist_noniid, cifar_iid,cifar_noniid
+from utils.options import args_parser
+from models.Update import LocalUpdate
+from models.Nets import MLP, CNNMnist, CNNCifar, CNNFemnist, CharLSTM
+from models.Fed import FedWeightAvg
+from models.test import test_img
+from utils.dataset import FEMNIST, ShakeSpeare
 
 
-class MLP(nn.Module):
-    def __init__(self, dim_in, dim_hidden, dim_out):
-        super(MLP, self).__init__()
-        self.layer_input = nn.Linear(dim_in, dim_hidden)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout()
-        self.layer_hidden = nn.Linear(dim_hidden, dim_out)
-        self.softmax = nn.Softmax(dim=1)
+if __name__ == '__main__':
 
-    def forward(self, x):
-        x = x.view(-1, x.shape[1]*x.shape[-2]*x.shape[-1])
-        x = self.layer_input(x)
-        x = self.dropout(x)
-        x = self.relu(x)
-        x = self.layer_hidden(x)
-        return self.softmax(x)
+    random.seed(123)
+    np.random.seed(123)
+    torch.manual_seed(123)
+    torch.cuda.manual_seed_all(123)
+    torch.cuda.manual_seed(123)
+    # parse args
+    args = args_parser()
+    args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
+
+    # load dataset and split users
+    if args.dataset == 'mnist':
+        trans_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+        dataset_train = datasets.MNIST('./data/mnist/', train=True, download=True, transform=trans_mnist)
+        dataset_test = datasets.MNIST('./data/mnist/', train=False, download=True, transform=trans_mnist)
+        # sample users
+        if args.iid:
+            dict_users = mnist_iid(dataset_train, args.num_users)
+        else:
+            dict_users = mnist_noniid(dataset_train, args.num_users)
+    elif args.dataset == 'cifar':
+        #trans_cifar = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        trans_cifar_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+        trans_cifar_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+        dataset_train = datasets.CIFAR10('./data/cifar', train=True, download=True, transform=trans_cifar_train)
+        dataset_test = datasets.CIFAR10('./data/cifar', train=False, download=True, transform=trans_cifar_test)
+        if args.iid:
+            dict_users = cifar_iid(dataset_train, args.num_users)
+        else:
+            dict_users = cifar_noniid(dataset_train, args.num_users)
+    elif args.dataset == 'fashion-mnist':
+        trans_fashion_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+        dataset_train = datasets.FashionMNIST('./data/fashion-mnist', train=True, download=True,
+                                              transform=trans_fashion_mnist)
+        dataset_test  = datasets.FashionMNIST('./data/fashion-mnist', train=False, download=True,
+                                              transform=trans_fashion_mnist)
+        if args.iid:
+            dict_users = mnist_iid(dataset_train, args.num_users)
+        else:
+            dict_users = mnist_noniid(dataset_train, args.num_users)
+    elif args.dataset == 'femnist':
+        dataset_train = FEMNIST(train=True)
+        dataset_test = FEMNIST(train=False)
+        dict_users = dataset_train.get_client_dic()
+        args.num_users = len(dict_users)
+        if args.iid:
+            exit('Error: femnist dataset is naturally non-iid')
+        else:
+            print("Warning: The femnist dataset is naturally non-iid, you do not need to specify iid or non-iid")
+    elif args.dataset == 'shakespeare':
+        dataset_train = ShakeSpeare(train=True)
+        dataset_test = ShakeSpeare(train=False)
+        dict_users = dataset_train.get_client_dic()
+        args.num_users = len(dict_users)
+        if args.iid:
+            exit('Error: ShakeSpeare dataset is naturally non-iid')
+        else:
+            print("Warning: The ShakeSpeare dataset is naturally non-iid, you do not need to specify iid or non-iid")
+    else:
+        exit('Error: unrecognized dataset')
+    img_size = dataset_train[0][0].shape
+
+    # build model
+    if args.model == 'cnn' and args.dataset == 'cifar':
+        net_glob = CNNCifar(args=args).to(args.device)
+    elif args.model == 'cnn' and (args.dataset == 'mnist' or args.dataset == 'fashion-mnist'):
+        net_glob = CNNMnist(args=args).to(args.device)
+    elif args.dataset == 'femnist' and args.model == 'cnn':
+        net_glob = CNNFemnist(args=args).to(args.device)
+    elif args.dataset == 'shakespeare' and args.model == 'lstm':
+        net_glob = CharLSTM().to(args.device)
+    elif args.model == 'mlp':
+        len_in = 1
+        for x in img_size:
+            len_in *= x
+        net_glob = MLP(dim_in=len_in, dim_hidden=64, dim_out=args.num_classes).to(args.device)
+    else:
+        exit('Error: unrecognized model')
 
 
-class CNNMnist(nn.Module):
-    def __init__(self, args):
-        super(CNNMnist, self).__init__()
-        self.conv1 = nn.Conv2d(args.num_channels, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, args.num_classes)
+    print(net_glob)
+    net_glob.train()
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+    # copy weights
+    w_glob = net_glob.state_dict()
 
+    # training
+    acc_test = []
+    clients = [LocalUpdate(args=args, dataset=dataset_train, idxs=dict_users[idx])
+               for idx in range(args.num_users)]
+    m, clients_index_array = max(int(args.frac * args.num_users), 1), range(args.num_users)
+    for iter in range(args.epochs):
+        w_locals, loss_locals, weight_locols= [], [], []
+        idxs_users = np.random.choice(clients_index_array, m, replace=False)
+        for idx in idxs_users:
+            w, loss = clients[idx].train(net=copy.deepcopy(net_glob).to(args.device))
+            w_locals.append(copy.deepcopy(w))
+            loss_locals.append(copy.deepcopy(loss))
+            weight_locols.append(len(dict_users[idx]))
 
-class CNNCifar(nn.Module):
-    def __init__(self, args):
-        super(CNNCifar, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=48, kernel_size=(3,3), padding=(1,1))
-        self.conv2 = nn.Conv2d(in_channels=48, out_channels=96, kernel_size=(3,3), padding=(1,1))
-        self.conv3 = nn.Conv2d(in_channels=96, out_channels=192, kernel_size=(3,3), padding=(1,1))
-        self.conv4 = nn.Conv2d(in_channels=192, out_channels=256, kernel_size=(3,3), padding=(1,1))
-        self.pool = nn.MaxPool2d(2,2)
-        self.fc1 = nn.Linear(in_features=8*8*256, out_features=512)
-        self.fc2 = nn.Linear(in_features=512, out_features=64)
-        self.Dropout = nn.Dropout(0.25)
-        self.fc3 = nn.Linear(in_features=64, out_features=10)
+        # update global weights
+        w_glob = FedWeightAvg(w_locals, weight_locols)
+        # copy weight to net_glob
+        net_glob.load_state_dict(w_glob)
 
-    def forward(self, x):
-        x = F.relu(self.conv1(x)) #32*32*48
-        x = F.relu(self.conv2(x)) #32*32*96
-        x = self.pool(x) #16*16*96
-        x = self.Dropout(x)
-        x = F.relu(self.conv3(x)) #16*16*192
-        x = F.relu(self.conv4(x)) #16*16*256
-        x = self.pool(x) # 8*8*256
-        x = self.Dropout(x)
-        x = x.view(-1, 8*8*256) # reshape x
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.Dropout(x)
-        x = self.fc3(x)
-        return x
+        # print accuracy
+        net_glob.eval()
+        acc_t, loss_t = test_img(net_glob, dataset_test, args)
+        print("Round {:3d},Testing accuracy: {:.2f}".format(iter, acc_t))
 
+        acc_test.append(acc_t.item())
 
-class CNNFemnist(nn.Module):
-    def __init__(self, args):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, 7, padding=3)
-        self.act = nn.ReLU()
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.out = nn.Linear(64 * 7 * 7, 62)
+    rootpath = './log'
+    if not os.path.exists(rootpath):
+        os.makedirs(rootpath)
+    accfile = open(rootpath + '/accfile_fed_{}_{}_{}_iid{}.dat'.
+                   format(args.dataset, args.model, args.epochs, args.iid), "w")
 
-    def forward(self, x):
-        x = x.reshape(-1, 1, 28, 28)
-        x = self.pool(self.act(self.conv1(x)))
-        x = self.pool(self.act(self.conv2(x)))
-        x = x.flatten(1)
-        # return self.dense2(self.act(self.dense1(x)))
-        return self.out(x)
+    for ac in acc_test:
+        sac = str(ac)
+        accfile.write(sac)
+        accfile.write('\n')
+    accfile.close()
+
+    # plot loss curve
+    plt.figure()
+    plt.plot(range(len(acc_test)), acc_test)
+    plt.ylabel('test accuracy')
+    plt.savefig(rootpath + '/fed_{}_{}_{}_C{}_iid{}_acc.png'.format(args.dataset, args.model, args.epochs, args.frac, args.iid))
 
 
-class CharLSTM(nn.Module):
-    def __init__(self):
-        super(CharLSTM, self).__init__()
-        self.embed = nn.Embedding(80, 8)
-        self.lstm = nn.LSTM(8, 256, 2, batch_first=True)
-        # self.h0 = torch.zeros(2, batch_size, 256).requires_grad_()
-        self.drop = nn.Dropout()
-        self.out = nn.Linear(256, 80)
-
-    def forward(self, x):
-        x = self.embed(x)
-        # if self.h0.size(1) == x.size(0):
-        #     self.h0.data.zero_()
-        #     # self.c0.data.zero_()
-        # else:
-        #     # resize hidden vars
-        #     device = next(self.parameters()).device
-        #     self.h0 = torch.zeros(2, x.size(0), 256).to(device).requires_grad_()
-        x, hidden = self.lstm(x)
-        x = self.drop(x)
-        # x = x.contiguous().view(-1, 256)
-        # x = x.contiguous().view(-1, 256)
-        return self.out(x[:, -1, :])
-
-    # def init_hidden(self, batch_size):
-    #     weight = next(self.parameters()).data
-    #
-    #     initial_hidden = (weight.new(2, batch_size, 256).zero_(),
-    #                       weight.new(2, batch_size, 256).zero_())
-    #
-    #     return initial_hidden
